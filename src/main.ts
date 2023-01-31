7bfd5a67e9d45505d565d254d6ac5160a51f7051
Zack DeRose <zack.derose@gmail.com>
2023-01-30 22:03:10 -0700

    add example

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,7 @@
-import express from 'express';
-
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+setTimeout(() => {
+  console.log('in setTimeout');
 });
 
-app.listen(port, () => {
-  console.log(`[ ready ] http://localhost:${port}`);
-});
+for (let i = 0; i < 1000; i++) {
+  console.log(i);
+}
